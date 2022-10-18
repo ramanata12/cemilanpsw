fa36d76b27695196f694a273333d7d3710bd6e8b
ramanata12 <ramadanawinata@gmail.com>
2022-10-18 16:40:56 +0700

    Membuat JPA controller

---
+++ src/main/java/cemilan/pws/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package cemilan.pws.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
