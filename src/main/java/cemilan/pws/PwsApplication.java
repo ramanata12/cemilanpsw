43ca605ee2dc485b78d5954872210e0045654d7a
ramanata12 <ramadanawinata@gmail.com>
2022-10-18 16:30:33 +0700

    Import file

---
+++ src/main/java/cemilan/pws/PwsApplication.java
@@ -0,0 +1,13 @@
+package cemilan.pws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cemilan/pws/PwsApplicationTests.java
@@ -0,0 +1,13 @@
+package cemilan.pws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
